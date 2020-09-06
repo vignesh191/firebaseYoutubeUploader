efe18d90b91df1290f4410214d1b41c8486e140e
vignesh191 <70774793+vignesh191@users.noreply.github.com>
2020-09-06 12:05:30 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,67 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+const fs = require('fs');
+const os = require('os')
+const path = require('path');
+const {google} = require('googleapis');
+const youtube = google.youtube('v3');
+
+admin.initializeApp();
+
+
+
+exports.YTUpload = functions.storage.object().onFinalize(async (object) => { //onFinalize is same as "on a db change"
+      const filePath = object.name   //uploaded file's metadata
+      const fileBucket = object.bucket
+      const bucket = admin.storage().bucket(fileBucket);
+      const fileName = path.basename(filePath);
+
+      if (!object.contentType.startsWith('video/')) { //for non-video files
+        console.log("File type is not a video.")
+        return
+      }
+
+      if (object.resourceState === 'not_exists') { //in the case of immediate file deletion
+        console.log("This is a deletion event.")
+        return
+      }
+
+      const tempFilePath = path.join(os.tmpdir(), fileName); //creating a temp path in Firebase VM
+      await bucket.file(filePath).download({destination: tempFilePath}); //downloading to temp path
+
+      //Visit "Google Developers Console" and create an OAuth2 Key to obtain these details
+      const oauth2 = new google.auth.OAuth2("YOUR_CLIENT_ID", "YOUR_CLIENT_SECRET", "YOUR_REDIRECT_URIS[0]");
+      oauth2.setCredentials({
+          refresh_token: "YOUR_REFRESH_TOKE"
+      })
+      google.options({auth: oauth2});
+      console.log('passed authentication phase')
+
+      const res = await youtube.videos.insert( //adding video to youtube channel
+        {
+          part: 'id,snippet,status',
+          notifySubscribers: false,
+          requestBody: {
+            snippet: {
+              title: 'Node.js YouTube Upload Test',
+              description: 'Testing YouTube upload via Google APIs Node.js Client',
+            },
+            status: {
+              privacyStatus: 'private',
+            },
+          },
+          media: {
+            body: fs.createReadStream(tempFilePath),
+          },
+        },
+             {
+
+        }
+      );
+      console.log('\n\n');
+      console.log(res.data);
+      fs.unlinkSync(tempFilePath)
+      return res.data;
+
+})
